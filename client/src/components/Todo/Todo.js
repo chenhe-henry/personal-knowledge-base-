393073924e5602c0d2bca73f6be3b066f3265b6f
chenhe-henry <chenhe.elec@gmail.com>
2020-04-22 16:41:29 +1000

    add nav to todo

--- client/src/components/Todo/Todo.js
+++ client/src/components/Todo/Todo.js
@@ -1,9 +1,38 @@
-import React from "react";
-
+import React, { Fragment } from "react";
+import "./Todo.scss";
 class Todo extends React.Component {
   state = {};
   render() {
-    return <div>This is Todo list</div>;
+    const listing = (
+      <div>
+        <div>Hello</div>
+        <ul>
+          <li></li>
+        </ul>
+      </div>
+    );
+    return (
+      <Fragment>
+        <div>
+          <div className="navigation">
+            <h1 className="nav">
+              <a href="/" className="nav-link">
+                Home
+              </a>
+            </h1>
+            <h1 className="nav">
+              <a href="/todo" className="nav-link">
+                Todo
+              </a>
+            </h1>
+          </div>
+          <div>This is Todo list</div>
+          <input value="Add task"></input>
+          <button>Add</button>
+          {listing}
+        </div>
+      </Fragment>
+    );
   }
 }
 
